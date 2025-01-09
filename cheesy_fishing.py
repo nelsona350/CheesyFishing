bb251580da2dfff6e94609fad8d3c016618c5453
Buster McThunderstick <bigdaddy_dirtbag (at) mlb2k23.com>
2025-01-09 10:39:09 -0600

    Added record-keeping code

--- cheesy_fishing.py
+++ cheesy_fishing.py
@@ -39,6 +39,31 @@ class App:
         
         self.radius = 20
         
+        with open("resources/records.txt", "r") as infile:
+            try:
+                fish,weight,angler = infile.readline().strip().split(',')
+                self.carpRecord = [fish,weight,angler]
+                fish,weight,angler = infile.readline().strip().split(',')
+                self.bassRecord = [fish,weight,angler]
+                fish,weight,angler = infile.readline().strip().split(',')
+                self.bluegillRecord = [fish,weight,angler]
+                fish,weight,angler = infile.readline().strip().split(',')
+                self.catfishRecord = [fish,weight,angler]
+                fish,weight,angler = infile.readline().strip().split(',')
+                self.mosaRecord = [fish,weight,angler]
+            except:
+                weight = "0.0"
+                angler = "Mr. Fisher"
+                self.carpRecord = ["Carp",weight,angler]
+                self.bassRecord = ["Bass",weight,angler]
+                self.bluegillRecord = ["Bluegill",weight,angler]
+                self.catfishRecord = ["Catfish",weight,angler]
+                self.mosaRecord = ["Mosasaurus",weight,angler]
+                
+            
+        self.button2 = Button(root, text="View Records", command=self.viewRecords)
+        self.button2.place(x=10, y=10)
+        
     def onClick(self, event):
         #print('Clicked canvas at: ', event.x, event.y, event.widget)
         # self.fishWindow = tk.Toplevel(root)
@@ -67,6 +92,56 @@ class App:
                      self.caughtbass()
                      
                 break
+                
+    def viewRecords(self):
+        self.recordsWindow = tk.Toplevel(root)
+        self.recordsWindow.wm_title("Catch")
+        self.recordsWindow.grab_set()
+        
+        labels = []
+        labels.append(Label(self.recordsWindow, text="Species"))
+        labels[-1].grid(row=0, column=0)
+        labels.append(Label(self.recordsWindow, text="Weight"))
+        labels[-1].grid(row=0, column=1)
+        labels.append(Label(self.recordsWindow, text="Angler"))
+        labels[-1].grid(row=0, column=2)
+        
+        labels.append(Label(self.recordsWindow, text=self.carpRecord[0]))
+        labels[-1].grid(row=1, column=0)
+        labels.append(Label(self.recordsWindow, text=self.carpRecord[1]))
+        labels[-1].grid(row=1, column=1)
+        labels.append(Label(self.recordsWindow, text=self.carpRecord[2]))
+        labels[-1].grid(row=1, column=2)
+                
+        labels.append(Label(self.recordsWindow, text=self.bassRecord[0]))
+        labels[-1].grid(row=2, column=0)
+        labels.append(Label(self.recordsWindow, text=self.bassRecord[1]))
+        labels[-1].grid(row=2, column=1)
+        labels.append(Label(self.recordsWindow, text=self.bassRecord[2]))
+        labels[-1].grid(row=2, column=2)
+                
+        labels.append(Label(self.recordsWindow, text=self.bluegillRecord[0]))
+        labels[-1].grid(row=3, column=0)
+        labels.append(Label(self.recordsWindow, text=self.bluegillRecord[1]))
+        labels[-1].grid(row=3, column=1)
+        labels.append(Label(self.recordsWindow, text=self.bluegillRecord[2]))
+        labels[-1].grid(row=3, column=2)
+                
+        labels.append(Label(self.recordsWindow, text=self.catfishRecord[0]))
+        labels[-1].grid(row=4, column=0)
+        labels.append(Label(self.recordsWindow, text=self.catfishRecord[1]))
+        labels[-1].grid(row=4, column=1)
+        labels.append(Label(self.recordsWindow, text=self.catfishRecord[2]))
+        labels[-1].grid(row=4, column=2)
+                
+        labels.append(Label(self.recordsWindow, text=self.mosaRecord[0]))
+        labels[-1].grid(row=5, column=0)
+        labels.append(Label(self.recordsWindow, text=self.mosaRecord[1]))
+        labels[-1].grid(row=5, column=1)
+        labels.append(Label(self.recordsWindow, text=self.mosaRecord[2]))
+        labels[-1].grid(row=5, column=2)
+                
+
 
     def caughtcarp(self):
         self.fishWindow = tk.Toplevel(root)
@@ -77,6 +152,15 @@ class App:
         label1.grid(row=0, column=0)
         label2 = Label(self.fishWindow, image=self.carp1)
         label2.grid(row=1, column=0)
+        if weight > float(self.carpRecord[1]):
+            self.carpRecord[1] = str(weight)
+            with open("resources/records.txt", "w") as outfile:
+                outfile.write(",".join(self.carpRecord)+"\n")
+                outfile.write(",".join(self.bassRecord)+"\n")
+                outfile.write(",".join(self.bluegillRecord)+"\n")
+                outfile.write(",".join(self.catfishRecord)+"\n")
+                outfile.write(",".join(self.mosaRecord)+"\n")
+            
         
     def caughtbass(self):
         self.fishWindow = tk.Toplevel(root)
@@ -87,6 +171,14 @@ class App:
         label1.grid(row=0, column=0)
         label2 = Label(self.fishWindow, image=self.bass1)
         label2.grid(row=1, column=0)        
+        if weight > float(self.bassRecord[1]):
+            self.bassRecord[1] = str(weight)
+            with open("resources/records.txt", "w") as outfile:
+                outfile.write(",".join(self.carpRecord)+"\n")
+                outfile.write(",".join(self.bassRecord)+"\n")
+                outfile.write(",".join(self.bluegillRecord)+"\n")
+                outfile.write(",".join(self.catfishRecord)+"\n")
+                outfile.write(",".join(self.mosaRecord)+"\n")
 
         
     def caughtbluegill(self):
@@ -98,6 +190,14 @@ class App:
         label1.grid(row=0, column=0)
         label2 = Label(self.fishWindow, image=self.bluegill1)
         label2.grid(row=1, column=0)
+        if weight > float(self.bluegillRecord[1]):
+            self.bluegillRecord[1] = str(weight)
+            with open("resources/records.txt", "w") as outfile:
+                outfile.write(",".join(self.carpRecord)+"\n")
+                outfile.write(",".join(self.bassRecord)+"\n")
+                outfile.write(",".join(self.bluegillRecord)+"\n")
+                outfile.write(",".join(self.catfishRecord)+"\n")
+                outfile.write(",".join(self.mosaRecord)+"\n")
                 
     def caughtcatfish(self):
         self.fishWindow = tk.Toplevel(root)
@@ -108,6 +208,14 @@ class App:
         label1.grid(row=0, column=0)
         label2 = Label(self.fishWindow, image=self.catfish1)
         label2.grid(row=1, column=0)
+        if weight > float(self.catfishRecord[1]):
+            self.catfishRecord[1] = str(weight)
+            with open("resources/records.txt", "w") as outfile:
+                outfile.write(",".join(self.carpRecord)+"\n")
+                outfile.write(",".join(self.bassRecord)+"\n")
+                outfile.write(",".join(self.bluegillRecord)+"\n")
+                outfile.write(",".join(self.catfishRecord)+"\n")
+                outfile.write(",".join(self.mosaRecord)+"\n")
                 
     def caughtmystery(self):
         self.fishWindow = tk.Toplevel(root)
@@ -120,6 +228,14 @@ class App:
         label1.grid(row=0, column=0)
         label2 = Label(self.fishWindow, image=self.mosa1)
         label2.grid(row=1, column=0)
+        if weight > float(self.mosaRecord[1]):
+            self.mosaRecord[1] = str(weight)
+            with open("resources/records.txt", "w") as outfile:
+                outfile.write(",".join(self.carpRecord)+"\n")
+                outfile.write(",".join(self.bassRecord)+"\n")
+                outfile.write(",".join(self.bluegillRecord)+"\n")
+                outfile.write(",".join(self.catfishRecord)+"\n")
+                outfile.write(",".join(self.mosaRecord)+"\n")
                 
 if __name__ == "__main__":
     root = tk.Tk()
